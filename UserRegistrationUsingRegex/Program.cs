a8428ce61221d9023e227fa9e075656bd12ae08d
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-17 20:05:15 +0530

    [Shivani] UC1 - Valid first name

--- UserRegistrationUsingRegex/Program.cs
+++ UserRegistrationUsingRegex/Program.cs
@@ -10,6 +10,24 @@ namespace UserRegistrationUsingRegex
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Chosee a oprtion");
+            Console.WriteLine("1. UC1 - Valid First Name");
+            Console.WriteLine("2. UC1 - Valid Last Name");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    UCOnePattern.ValidateFirstName();
+                    break;
+               /* case 2:
+                    UCTwoPattern.ValidateLastName();
+                    break;*/
+                default:
+                    Console.WriteLine("Choose a appropriate option");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
