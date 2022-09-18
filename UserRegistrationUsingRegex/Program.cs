667f060ca77b116c16d5bd96707d587224b291b9
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-18 13:34:19 +0530

    [Shivani] UC4 - Validate mobile number

--- UserRegistrationUsingRegex/Program.cs
+++ UserRegistrationUsingRegex/Program.cs
@@ -11,9 +11,10 @@ namespace UserRegistrationUsingRegex
         static void Main(string[] args)
         {
             Console.WriteLine("Chosee a oprtion");
-            Console.WriteLine("1. UC1 - Valid First Name");
-            Console.WriteLine("2. UC2 - Valid Last Name");
-            Console.WriteLine("3. UC3 - Valid Email Address");
+            Console.WriteLine("1. UC1 - Validate First Name");
+            Console.WriteLine("2. UC2 - Validate Last Name");
+            Console.WriteLine("3. UC3 - Validate Email Address");
+            Console.WriteLine("4. UC4 - Validate Mobile Number");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -26,6 +27,9 @@ namespace UserRegistrationUsingRegex
                     break;
                 case 3:
                     UCThreePattern.ValidarEmail();
+                    break;
+                case 4:
+                    UCFourPattern.ValidateMobileNumber();
                     break;
                 default:
                     Console.WriteLine("Choose a appropriate option");
