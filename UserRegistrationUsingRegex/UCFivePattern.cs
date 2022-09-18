a77ab4906639e119e4effad35f52ebc1b73db511
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-18 18:03:36 +0530

    [Shivani] UC5-Validate password, Rule1-Password should have mini 8 chars

---
+++ UserRegistrationUsingRegex/UCFivePattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationUsingRegex
+{
+    public class UCFivePattern
+    {
+        public static void validatePassword(string password)
+        {
+            
+            var hasLowerChar = new Regex("^(?=.*[a-z]).{8,}$");
+
+            if (!hasLowerChar.IsMatch(password))
+            {
+                Console.WriteLine("Password should have minimum 8 characters");
+            }
+            else
+            {
+                Console.WriteLine("Password is valid");
+            }
+        }
+    }
+}
