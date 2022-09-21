822d7ab71e7e709639ed1156ab2e4d6634853f7f
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-21 18:46:05 +0530

    [Shivani] Test Email samples

---
+++ UserRegistrationUsingRegex/NLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace UserRegistrationUsingRegex
+{
+    public class NLog
+    {
+
+        public static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void LogInfo(string msg)
+        {
+            logger.Info(msg);
+        }
+
+    }
+}
+
