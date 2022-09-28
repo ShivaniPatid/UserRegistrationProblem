b7899268bca19ba373af31757a75ff9a5fa53706
ShivaniPatid <shivanipatidar712@gmail.com>
2022-09-28 18:59:36 +0530

    [Shivani] Test case for UC11 Validate email address

---
+++ TestUserRegistration/TestEmailAddress.cs
@@ -0,0 +1,24 @@
+﻿using UserRegistrationUsingRegex;
+
+namespace TestUserRegistration
+{
+    [TestClass]
+    public class TestEmailAddress
+
+    {
+        [TestMethod]
+        public void TestingEmailAddress()
+        {
+            
+            string email = "abc@yahoo.com";
+            string message = "Valid Email Address";
+            UC11TestEmailAddress uc11 = new UC11TestEmailAddress();
+            string actual=uc11.ValidateEmailAddress(email);
+            Assert.AreEqual(actual,message);
+
+
+        }
+
+        
+    }
+}
